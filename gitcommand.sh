b2f3f6825076d33f0d979a9f8685c9b79fb5b7b3
Robin <robin113x@gmail.com>
2022-08-06 01:52:36 +0530

    git command is added

---
+++ gitcommand.sh
@@ -0,0 +1,24 @@
+git config --global user.name "Robin"
+git config --global user.email "Robin113x@gmail.com"
+git config push.default
+
+git clone <repo url>  <distion path>
+
+git status
+git add .
+git commit -m "Proper message"
+
+git log --> show log/commit id (40 char SHA-1 Hash)/message
+git log -1  --> show lastet 1st log only
+git log -3  --> show lastet 3 log only
+git log --oneline --> show log in short 
+git log --oneline -1 --> show log in short 
+
+#show details of commit 
+git show <commit id>
+
+[Git consider file only not directory]
+
+
+
+
